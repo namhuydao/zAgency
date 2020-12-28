41773e1e5979dc8fd33df69a0cbec96b9f2dd44e
namhuydao <namhuydao@tutamail.com>
2020-12-28 17:29:25 +0700

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+let bar = document.querySelector(".header__menu-bar");
+let menulist = document.querySelector(".header__menu-list");
+bar.addEventListener('click', () =>{
+  menulist.classList.toggle('show')
+  $(".fa-bars").toggleClass("fa-bars fa-times");
+})
+let pages = document.querySelector(".pages");
+let dropdown = document.querySelector(".dropdown");
+pages.addEventListener('click', () =>{
+  dropdown.classList.toggle('show')
+})
