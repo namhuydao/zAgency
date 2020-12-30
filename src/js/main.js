bdac1276f13779d1d362ad574e79bb9f766d8967
namhuydao <namhuydao.tutamail.com>
2020-12-30 12:41:58 +0700

    add files

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
+//Window onload
+$(window).on('load', function(event) {
+  $('.onload').delay(1000).fadeOut('fast');
+});
+
+let header = document.querySelector("#header");
+window.addEventListener('scroll', () => {
+    header.classList.toggle('sticky', window.scrollY > 0)
+})
 let bar = document.querySelector(".header__menu-bar");
 let menulist = document.querySelector(".header__menu-list");
 bar.addEventListener('click', () =>{
