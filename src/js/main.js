48409e98041832278052810d9416dc43e3780596
namhuydao <namhuydao.tutamail.com>
2020-12-30 18:51:19 +0700

    add files

--- src/js/main.js
+++ src/js/main.js
@@ -11,10 +11,38 @@ let bar = document.querySelector(".header__menu-bar");
 let menulist = document.querySelector(".header__menu-list");
 bar.addEventListener('click', () =>{
   menulist.classList.toggle('show')
-  $(".fa-bars").toggleClass("fa-bars fa-times");
+  $(".btnToggle").toggleClass("fa-bars fa-times");
 })
 let pages = document.querySelector(".pages");
 let dropdown = document.querySelector(".dropdown");
 pages.addEventListener('click', () =>{
   dropdown.classList.toggle('show')
 })
+
+// Active isotope with jQuery code:
+
+var $grid = $('.grid').imagesLoaded(function() {
+	// init Isotope after all images have loaded
+	$grid.isotope({
+		itemSelector    : '.grid-item',
+		percentPosition : true,
+    gutter          : 0,
+    transformsEnabled: false,
+		masonry         : {
+			// use outer width of grid-sizer for columnWidth
+			columnWidth : '.grid-sizer'
+    }
+  });
+});
+
+// Isotope click function
+$('.iso-nav a').click(function() {
+	$('.iso-nav a').removeClass('active');
+	$(this).addClass('active');
+
+	var selector = $(this).attr('data-filter');
+	$('.grid').isotope({
+		filter : selector
+	});
+	return false;
+});
